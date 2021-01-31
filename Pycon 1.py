2ab07253530ba68fecc63f664857bf1494ee8032
SophieWorks <70182028+SophieWorks@users.noreply.github.com>
2021-01-30 20:55:28 -0500

    Add files via upload

--- Pycon 1.py
+++ Pycon 1.py
@@ -12,7 +12,7 @@ declining balance depreciation, and rate manipulation.
 @author: sophi
 """
 import numpy as np
-import math as mt
+import math
 import time 
 
 print('Assume 1 year annual interest',
@@ -22,10 +22,14 @@ print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
 def simpleInterest():
-    
+
+    print('\nInsert 1 if not used')
+    time.sleep(1)    
+
     P = float(input('Input present amount P:'))
-    i = float(input('Input interest rate i:'))
-    F = P*(1+i)
+    i = float(input('Input interest rate (i):'))/100
+    N = float(input('Input period:'))
+    F = P*(1+i*N)
     
     print('\nFuture Amount is:',F)
     
@@ -33,17 +37,34 @@ def simpleInterest():
     main()
 
 def compoundInterest():
-    P = float(input('Input present amount P:'))
-    i = float(input('Input interest rate i:'))
-    N = float(input('Input period:'))
-    F = P*(1+i)**N
     
-    print('\nFuture Amount is:',F)
+    print('\nInsert 1 if not used')
+    time.sleep(1)
+    
+    P = float(input('Input present amount P:'))
+    i = float(input('Input interest rate (i):'))/100
+    N = float(input('Input period in years (n):'))
+    F = float(input('Input Future Amount:'))
+    
+    if F==1:
+        F = P*(1+i)**N
+        print('\nFuture Amount is:',F)
+    
+    if P==1:
+        P = F/((1+i)**N)
+        print('\nNeeded money today is', P)    
+    
+
 
     time.sleep(3)
     main()
 
 def rateManipulation():
+    
+    print('\nInsert 1 if not used')
+    time.sleep(1)
+    
+    ieff = float(input('Input effective interest (if q gives):'))/100
     
     oldPeriod = input('Changing from:'
                   ' \n(1-Daily, 2-Weekly, '
@@ -81,56 +102,191 @@ def rateManipulation():
         
     k = x/y
 
-    print(k)           
+    # print(k)           
     # P = float(input('Input present amount P:'))
-    r = float(input('Input nominal annual interest as decimal (r):'))
+    r = float(input('Input nominal annual interest (r):'))/100
     m = float(input('Input compounding period (m):'))
     # k = float(input('Input how many {x} in thing:'.format(x=oldPeriod)))
     
     iek = ((1+r/m)**k)-1
     
+    # if m==1:
+    #     m = np.log(ieff+1)/np.log*()
+    
     # print(iek)
-    print('\nNew rate manipulation is: ',iek)
+    print('\nNew rate manipulation is: ',iek*100, '\N{PERCENT SIGN}')
 
-    time.sleep(3)
+    time.sleep(2)
     main()
 
 
+
+def nomeffInterest():
+    print('\nInsert 1 if not used')
+    time.sleep(1)
+    
+    P = float(input('Input present amount (P):'))
+    r = float(input('Input nominal annual interest (r):'))/100
+    m = float(input('Input compounding period (m):'))
+    n = float(input('Input time in years (n):'))
+    F = float(input('Input future amount (F):'))
+    
+    isub = r/m 
+    N = m*n 
+    ieff = (((1+isub)**m)-1)
+    
+    if F==1:
+        F = P*(1+isub)**N
+        print('\nFuture amount is', F)
+    if r==0.01:
+        ieffNew = (F/P)**(1/n)
+        r = ((ieffNew)**(1/2)-1)*2
+        print('\nAnnual Nominal interest is', r)
+    if P ==1:
+        P = F/(((1+(ieff))**n)-1)
+        print('\nNeeded money today is', P)
+
+     
+    
+    # print('\nEffective interest rate is', ieff,'\N{PERCENT SIGN}')
+    
+    
+    time.sleep(2)
+    main()
+
+def contInterest():
+    
+    print('\nInsert 1 if not used')
+    time.sleep(1)
+    
+    P = float(input('Input present amount (P):'))
+    r = float(input('Input nominal annual interest (r):'))/100
+    n = float(input('Input time in years (n):'))
+    
+    
+    F = P*(math.exp(r*n))
+    ieff = (math.exp(r)-1)*100
+    print('\nEffective interest rate is', ieff,'\N{PERCENT SIGN}')
+    print('\nFuture amount is', F)
+
+    time.sleep(2)
+    main()
+
+
+def SLDepreciation():
+    N = int(input('Input useful life in years (N):'))
+    P = float(input('Input purchase price (P):'))
+    S = float(input('Input salvage value (S):'))
+    
+    dep = float((P-S)/N)
+    print('\nDepreciation is: ', dep)
+
+    i = np.arange(1,N+1,1)
+    
+    print('\n')
+    for n in i:
+        bv = P-(n*(dep))
+        ad = n*(dep)
+        print('Year {i}'.format(i=n))
+        print('Book Value: ',bv)
+        print('Accumulated Depreciation: ',ad)
+        print('\n')
+    
+    time.sleep(2)
+    main()
+    
+
+def DBDepreciation():
+
+    print('\nInsert 1 if not used')
+    time.sleep(1)
+
+    P = float(input('Input purchase price (P):'))
+    S = float(input('Input salvage value (S):'))
+    d = float(input('Input depreciation rate (d):'))/100 
+    N = int(input('Input useful life in years (N):'))
+    
+    
+    if d == 0.01:
+        d = 1- (S/P)**(1/N)
+        print('\nDep Rate is', d*100,'\N{PERCENT SIGN}')
+    
+    i = np.arange(1,N+1,1)
+    
+    print('\n')
+    for n in i:
+        bv = P*(1-d)**n
+        bv_1 = P*(1-d)**(n-1)
+        dep = (bv_1)*d
+
+        print('\nYear {i}'.format(i=n))
+        print('Book Value is: ', bv)
+        print('Depreciation charge is: ', dep)
+
+    
+    
+    
+    
 """~~~~~~~~~MAIN FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
 def main():
-    fnc = input('Enter to call a function:\n'
-                '1 - Rate Manipulation\n' 
-                '2 - Simple Interest\n'
-                '3 - Compound Interest ')
-    if fnc =='1':
-        rateManipulation()
-    if fnc =='3':
-        compoundInterest()
-    if fnc =='2':
-        simpleInterest()
-        
+   
+    try:
+        running = True
+        while running:
+            fnc = int(input('Enter to call a function:\n'
+                        '1 - Rate Manipulation\n' 
+                        '2 - Simple Interest\n'
+                        '3 - Compound Interest\n'
+                        '4 - Nominal Compound Interest\n'
+                        '5 - Continuous Interest\n'
+                        '6 - SL Dep\n'
+                        '7 - DB Dep\n'
+                        '9 - Exit\n'))
+            if fnc ==1:
+                print('\nCalled Rate Manipulator!\n')
+                rateManipulation()
+                running = False
+            if fnc ==2:
+                print('\nCalled Simple Interest!\n')
+                simpleInterest()
+                running = False
+            if fnc ==3:
+                print('\nCalled Compound Interest!\n') 
+                compoundInterest()
+                running = False
+            if fnc ==4:
+                print('\nCalled Nominal Effective Comppound Interest!\n')
+                nomeffInterest()
+                running = False
+            if fnc ==5:
+                print('\nCalled Continuous Interest!\n')
+                contInterest()
+                running = False
+            if fnc ==6:
+                print('\nCalled SL Dep!\n')
+                SLDepreciation()
+                running = False
+            if fnc ==7:
+                print('\nCalled DB Dep!\n')
+                DBDepreciation()
+                running = False
+                
+                
+    
+            if fnc ==9:
+                print('\nExiting Function!')
+                running = False
+    except ValueError:
+        print('\nInput a valid number! Try again...')
+        main()
+            
 """~~~~~~~~~MAIN FUNCTION~end~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
 main()
-    # r = input('Input Nominal Annual Interest r:')
-    # m = input('Commounding Period m:')
-    # P = input('Input present amount of money')
-    # isub = r/m
-    # F = ('Future Amount = ')
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
     
     
 
